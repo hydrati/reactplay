67039cb28aa490c565829c4e5d2d3e85ab9d2e07
Hydrogen <3038094028@qq.com>
2022-03-13 13:05:03 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   esbuild: {
     jsx: 'transform',
-    jsxFactory: '__JSX__.h',
-    jsxFragment: '__JSX__.Fragment',
-    jsxInject: `import * as __JSX__ from '/src/jsx'`
+    jsxFactory: '__JSX__h',
+    jsxFragment: '__JSX__Fragment',
+    jsxInject: `import {h as __JSX__h,Fragment as __JSX__Fragment} from '/src/jsx'`
   },
   build: {
-    lib: {
-      entry: './src/reactivity.ts',
-      fileName: 'reactivity',
-      formats: ['es', 'iife'],
-      name: 'Reactivity'
-    }
+    // lib: {
+    //   entry: './src/reactivity.ts',
+    //   fileName: 'reactivity',
+    //   formats: ['es', 'iife'],
+    //   name: 'Reactivity'
+    // }
   }
 })
