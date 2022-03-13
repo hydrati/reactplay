04686ccb1a6fb072b85c77ef07bab2a800c749f4
Hydrogen <3038094028@qq.com>
2022-03-13 13:04:15 +0800

    feat(reactive): readonly

--- src/stores/reactive.ts
+++ src/stores/reactive.ts
@@ -1,19 +1,75 @@
-import { MapStore } from "./deps"
+import { effect, MapStore } from "./deps"
 import { RefImpl, RefStore, ShallowRefStore } from "./ref"
 
 let activeReactive = new WeakMap<any, MapStore>()
+let activeReadonly = new WeakMap<any, MapStore>()
 let activeReactiveTarget = new WeakMap<any, any>()
 
 let activeTargetReactive = new WeakMap<any, any>()
 let activeTargetShallowReactive = new WeakMap<any, any>()
+let activeTargetReadonly = new WeakMap<any, any>()
+let activeTargetShallowReadonly = new WeakMap<any, any>()
 
+const ReactiveStore = Symbol("Reactive")
 
 export function isReactive<T extends object>(o: T): boolean {
   return activeReactive.has(o)
 }
 
-function createReactive<T extends object>(o: T, shallow = false): T {
-  if (isReactive(o)) {
+export function isReadonly<T extends object>(o: T): boolean {
+  return activeReadonly.has(o)
+}
+
+function createReadonly<T extends object>(o: T, shallow = false, parent?: MapStore): T {
+  if (isReadonly(o) || typeof o !== 'object') {
+    return o
+  }
+
+  if (shallow) {
+    const proxy = activeTargetShallowReadonly.get(o)
+    if (proxy !== undefined) return proxy
+  } else {
+    const proxy = activeTargetReadonly.get(o)
+    if (proxy !== undefined) return proxy
+  }
+
+  const store = new MapStore(parent)
+  const proxy = new Proxy(o, {
+    get(target, p, r) {
+      let value: any
+      if (!store.has(p)) {
+        effect(() => {
+          value = Reflect.get(target, p, r)
+          store.effect(p)
+        })
+      } else {
+        value = Reflect.get(target, p, r)
+      }
+      store.collect(p)
+      return shallow ? value : createReadonly(value, false, store)
+    },
+    set() {
+      return false
+    },
+    deleteProperty() {
+      return false
+    }
+  } as ProxyHandler<T>)
+
+  activeReadonly.set(proxy, store)
+  activeReactiveTarget.set(proxy, o)
+  
+  if (shallow) {
+    activeTargetShallowReadonly.set(o, proxy)
+  } else {
+    activeTargetReadonly.set(o, proxy)
+  }
+
+  return proxy
+}
+
+function createReactive<T extends object>(o: T, shallow = false, parent: MapStore | null = null): T {
+  if (isReactive(o) || typeof o !== 'object') {
     return o
   }
 
@@ -25,11 +81,12 @@ function createReactive<T extends object>(o: T, shallow = false): T {
     if (proxy !== undefined) return proxy
   }
 
-  const store = new MapStore()
+  const store = new MapStore(parent)
   const proxy = new Proxy(o, {
     get(target, p, r) {
       store.collect(p)
-      return Reflect.get(target, p, r)
+      const value = Reflect.get(target, p, r)
+      return shallow ? value : createReactive(value, false, store)
     },
     set(target, p, v, r) {
       if (typeof v === "object" && !isReactive(v) && !shallow) {
@@ -49,7 +106,7 @@ function createReactive<T extends object>(o: T, shallow = false): T {
 
       return ret
     }
-  })
+  } as ProxyHandler<T>)
 
   activeReactive.set(proxy, store)
   activeReactiveTarget.set(proxy, o)
@@ -76,6 +133,22 @@ export function getReactiveStore<T extends object>(o: T) {
 }
 
 export function getReactiveTarget<T extends object>(o: T) {
+  return activeReactiveTarget.get(o)
+}
+
+export function readonly<T extends object = {}>(o: T): Readonly<T> {
+  return createReadonly(o)
+}
+
+export function shallowReadonly<T extends object = {}>(o: T): Readonly<T> {
+  return createReadonly(o, true)
+}
+
+export function getReadonlyStore<T extends object>(o: T) {
+  return activeReadonly.get(o)
+}
+
+export function getReadonlyTarget<T extends object>(o: T) {
   return activeReactiveTarget.get(o)
 }
 
