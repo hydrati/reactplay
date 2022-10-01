54582f9702c0c531cda4d69de50a40dc5acd8c35
Hydrogen <3038094028@qq.com>
2022-10-01 22:47:38 +0800

    chore: sync

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { useEffect, useDetachedScope } from './reactive'
+import { useEffect, useDetachedScope, onScopeDipose } from './reactive'
 import { useSignal } from './reactive/functional'
 
 const count = useSignal(0)
 
 const stop = useDetachedScope(() => {
   useEffect(() => console.log(count())) // 0
+  onScopeDipose(() => console.log('Scope Diposed!'))
 })
 
 count.set(1) // 1
